880a7be4177607910ec3da7b2ea80f3474976d15
Alfonso Rios <mr_moon29@srmoon29.local>
2022-06-03 11:38:16 -0500

    Add initial App.js and dependencies package.json

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Alert, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import LoginView from './views/LoginView';
+import HomeView from './views/HomeView';
+import * as SecureStore from 'expo-secure-store';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            page: 'login'
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            const token = await SecureStore.getItemAsync('secure_token');
+            if(token) {
+                this.setState({page: 'home'});
+            }
+        } catch (e) {
+            if(e) {
+                Alert.alert('ERROR', `${JSON.stringify(e)}`);
+            }
+        }
+    }
+
+    loginSuccess(payload) {
+        SecureStore.setItemAsync('secure_token', payload.token)
+            .then(() => {
+                this.setState({page: 'home'});
+            })
+            .catch(error => {
+                Alert.alert('ERROR', `${JSON.stringify(error)}`);
+            });
+    }
+
+    loginError(error) {
+        Alert.alert('Error', `${JSON.stringify(error)}`);
+    }
+
+    async logout() {
+        await SecureStore.deleteItemAsync('secure_token');
+        this.setState({page: 'login'});
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <ScrollView>
+                    {this.state.page === 'login' && (
+                        <LoginView
+                            loginSuccess={this.loginSuccess.bind(this)}
+                            loginError={this.loginError.bind(this)}
+                        ></LoginView>
+                    )}
+                    {this.state.page === 'home' && (
+                        <HomeView
+                            logout={this.logout.bind(this)}>
+                        </HomeView>
+                    )}
+                </ScrollView>
+            </SafeAreaView>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+    },
 });
